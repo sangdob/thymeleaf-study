f786010c172485f0ad825d9ce133047e924f8d8c
sangdob <51118110+sangdob@users.noreply.github.com>
2022-04-04 23:33:01 +0900

    타임리프 - 연산
    타임리프의 기능중 기본연산을 이용하여 연산하는 방법을 나타낸다.

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -90,4 +90,11 @@ public class BasicController {
 
 		return "basic/literal";
 	}
+
+	@GetMapping("operation")
+	public String operation(Model model) {
+		model.addAttribute("nullData", null);
+		model.addAttribute("data", "spring!");
+		return "basic/operation";
+	}
 }
