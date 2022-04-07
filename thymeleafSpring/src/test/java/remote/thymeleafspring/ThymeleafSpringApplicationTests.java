227341313e7a154f6f14f4bc21f2f870516c9f01
sangdob <51118110+sangdob@users.noreply.github.com>
2022-04-08 03:27:13 +0900

    thymeleafspring start

---
+++ thymeleafSpring/src/test/java/remote/thymeleafspring/ThymeleafSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package remote.thymeleafspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
